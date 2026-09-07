8308748dbe22b62e7b22c03ae1803aed5ed66ffc
Og Elvis <127100981+ogelvis@users.noreply.github.com>
2026-09-07 19:26:34 +0100

    Delete prisma directory

--- prisma/migrations/20260906000000_notification_link/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Notification" ADD COLUMN "link" TEXT;
-
--- CreateIndex
-CREATE INDEX "Notification_userId_readAt_idx" ON "Notification"("userId", "readAt");
--- prisma/migrations/20260907190000_add_response_metrics/migration.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "ServiceRequest"
-ADD COLUMN "requiresProfessionalResponse" BOOLEAN NOT NULL DEFAULT true,
-ADD COLUMN "responseExcluded" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN "responseExclusionReason" TEXT,
-ADD COLUMN "responseAvailableAt" TIMESTAMP(3),
-ADD COLUMN "professionalRespondedAt" TIMESTAMP(3);
